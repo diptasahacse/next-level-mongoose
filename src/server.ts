c71334117b46da6282ad9cf5b588ed468473f947
diptasahacse <diptasaha.lpu.cse@gmail.com>
2023-05-12 23:57:18 +0600

    connect local database with mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,22 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+// getting-started.js
+const mongoose = require("mongoose");
+const app = express();
+const port = 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+async function main() {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/explore-mongoose"); //explore-mongoose is the name of the database
+    console.log("Database is connected");
+  } catch (error) {
+    console.log("Failed to connect database");
+  }
+}
+main()
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Server is listening on port ${port}`);
+});
